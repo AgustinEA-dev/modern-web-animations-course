942242842f172b6be991332d428944b7014953a7
unknown <ag_antro@hotmail.com>
2024-11-16 14:12:37 -0300

    added more keyframes

--- animations/index.js
+++ animations/index.js
@@ -1,7 +1,21 @@
+//Animated squares.
+
 const puaseButton = document.querySelector(".pause")
 const square = document.querySelectorAll(".square")
 
-document.addEventListener("click", () => {
+puaseButton.addEventListener("click", () => {
     square.forEach((square) => square.classList.toggle("paused"))
     console.log("clicked")
 })
+
+//Color board.
+
+const boardContainer = document.querySelector(".color-board")
+
+for (let i = 0; i < 21; i++) {
+    const boardSquare = document.createElement("div")
+    boardSquare.classList.add("board-square")
+    boardContainer.appendChild(boardSquare)
+    console.log(boardSquare)
+
+}
