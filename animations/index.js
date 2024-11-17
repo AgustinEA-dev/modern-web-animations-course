ee9dbf8dd7364fa701b5c15557d1c0f4cf22b761
unknown <ag_antro@hotmail.com>
2024-11-17 20:53:55 -0300

    added reset board button

--- animations/index.js
+++ animations/index.js
@@ -1,21 +1,39 @@
-//Animated squares.
+//Animated squares/Keyframes
 
 const puaseButton = document.querySelector(".pause")
 const square = document.querySelectorAll(".square")
 
 puaseButton.addEventListener("click", () => {
     square.forEach((square) => square.classList.toggle("paused"))
-    console.log("clicked")
 })
 
-//Color board.
+//Color board/EventDelegation
 
 const boardContainer = document.querySelector(".color-board")
+const resetBoardButton = document.querySelector(".reset-button")
 
 for (let i = 0; i < 21; i++) {
     const boardSquare = document.createElement("div")
     boardSquare.classList.add("board-square")
     boardContainer.appendChild(boardSquare)
-    console.log(boardSquare)
-
 }
+
+function addGlobalEventListener(type, selector, callback) {
+    document.addEventListener(type, (e) => {
+        if (e.target.matches(selector)) {
+            callback(e)
+        }
+    })
+}
+
+addGlobalEventListener("click", ".board-square", (e) => {
+    e.target.classList.toggle("board-square-flip")
+})
+
+resetBoardButton.addEventListener("click", () => {
+    const allSquares = document.querySelectorAll(".board-square")
+    allSquares.forEach(square => square.classList.remove("board-square-flip"))
+})
+
+
+
